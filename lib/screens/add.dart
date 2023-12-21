c678a0f2a1bc5af13424fa3919e32e6889fcb622
Kwame Agyapong <Kwameagyapong91@gmail.com>
2023-12-21 22:18:52 +0000

    completd dropdown

--- lib/screens/add.dart
+++ lib/screens/add.dart
@@ -10,6 +10,8 @@ class AddScreen extends StatefulWidget {
 }
 
 class _AddScreenState extends State<AddScreen> {
+  FocusNode focusNode = FocusNode();
+  final TextEditingController _textEditingController = TextEditingController();
   String? selectedItem;
   final List<String> _item = [
     'Food',
@@ -21,6 +23,15 @@ class _AddScreenState extends State<AddScreen> {
     'Education',
     'Other'
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    focusNode.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,39 +63,114 @@ class _AddScreenState extends State<AddScreen> {
           const SizedBox(
             height: 50,
           ),
+          dropDown(),
+          const SizedBox(
+            height: 20,
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              width: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.grey.shade300),
-              ),
-              child: DropdownButton<String>(
-                items: _item
-                    .map((e) => DropdownMenuItem(
-                          value: e,
-                          child: Container(),
-                        ))
-                    .toList(),
-                hint: ModifiedText(
-                  text: " Name",
-                  color: AppColors.black,
-                  fontSize: 22,
-                ),
-                dropdownColor: Colors.white,
-                isExpanded: true,
-                underline: const SizedBox(),
-                onChanged: ((value) {
-                  setState(() {
-                    selectedItem = value!;
-                  });
-                }),
-              ),
-            ),
-          )
+            child: TextField(
+                focusNode: focusNode,
+                controller: _textEditingController,
+                decoration: InputDecoration(
+                  hintText: "Amount",
+                  labelStyle: TextStyle(
+                    color: Colors.grey.shade500,
+                    fontSize: 22,
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade500,
+                    fontSize: 22,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide:
+                        BorderSide(color: AppColors.lightWhite, width: 2),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade200,
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: AppColors.black, width: 2),
+                  ),
+                )),
+          ),
         ],
+      ),
+    );
+  }
+
+  Padding dropDown() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey.shade300),
+        ),
+        child: DropdownButton<String>(
+          value: selectedItem,
+          onChanged: ((value) {
+            setState(() {
+              selectedItem = value!;
+            });
+          }),
+          items: _item
+              .map((e) => DropdownMenuItem(
+                    value: e,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.circle,
+                          color: AppColors.bgColor,
+                          size: 10,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        ModifiedText(
+                          text: e,
+                          color: AppColors.black,
+                          fontSize: 22,
+                        ),
+                      ],
+                    ),
+                  ))
+              .toList(),
+          selectedItemBuilder: (BuildContext context) => _item
+              .map(
+                (e) => Row(
+                  children: [
+                    Icon(
+                      Icons.circle,
+                      color: AppColors.bgColor,
+                      size: 10,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    ModifiedText(
+                      text: e,
+                      color: AppColors.black,
+                      fontSize: 22,
+                    ),
+                  ],
+                ),
+              )
+              .toList(),
+          hint: ModifiedText(
+            text: " Name",
+            color: AppColors.black,
+            fontSize: 22,
+          ),
+          dropdownColor: Colors.white,
+          isExpanded: true,
+          underline: const SizedBox(),
+        ),
       ),
     );
   }
